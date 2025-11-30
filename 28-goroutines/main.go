8eb1c77953a0894791561592dd91a7da8f318079
Ulises <ulisesapaza.280@gmail.com>
2025-11-29 22:12:51 -0300

    goroutines

---
+++ 28-goroutines/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := range 3 {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
